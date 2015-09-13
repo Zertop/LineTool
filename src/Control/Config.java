d82ac0599149ae149c10b6ce5ba9a43b8adc66a5
Zertop <info@zertop.com>
2015-09-13 18:31:53 +0200

    Begin Work on Version 2.2.0

--- src/Control/Config.java
+++ src/Control/Config.java
@@ -4,7 +4,7 @@ package Control;
 
 public class Config {
 
-    private static final int version = 219;
+    private static final int version = 220;
     private static final String releasesURL = "http://releases.zertop.com/linetool/";
 
     public static int getVersion() {
